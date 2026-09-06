84097dc020207355af0bc707a2783a1c45dc04c8
PratikC54 <pratiksenapati54@gmail.com>
2026-09-06 22:42:36 +0530

    Main spring boot application

---
+++ extension/src/main/java/com/infera/extension/controller/ResearchController.java
@@ -0,0 +1,10 @@
+package com.infera.extension.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/research")
+public class ResearchController {
+
+}
