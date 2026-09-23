4fdddd8fdce632359cb92d42925c6f146e999135
PratikC54 <pratiksenapati54@gmail.com>
2026-09-23 22:07:44 +0530

    Added CORS and process end-point

--- extension/src/main/java/com/infera/extension/controller/ResearchController.java
+++ extension/src/main/java/com/infera/extension/controller/ResearchController.java
@@ -1,10 +1,26 @@
 package com.infera.extension.controller;
 
+import com.infera.extension.dto.ResearchRequest;
+import com.infera.extension.service.ResearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/research")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class ResearchController {
+
+    private final ResearchService service;
+
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest request){
+        String result = service.processRequest(request);
+        return ResponseEntity.ok(result);
+    }
+
 
 }
